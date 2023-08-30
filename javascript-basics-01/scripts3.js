510b8bac8444f316d7d4c41cb312eaa23488e1d3
souhad <souhadmoussa86@gmail.com>
2023-08-30 10:48:40 +0300

    step 3

---
+++ javascript-basics-01/scripts3.js
@@ -0,0 +1,6 @@
+var userName = prompt("Please enter your name:");
+if (userName !== null && userName !== "") {
+    alert("Hello, " + userName);
+} else {
+    alert("Hello there! Nice to meet you.");
+}
