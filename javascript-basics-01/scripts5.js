34ebab0dfa2d3c36349c8147dbd51ccf25ba37c4
souhad <souhadmoussa86@gmail.com>
2023-08-30 13:16:50 +0300

    step 5

---
+++ javascript-basics-01/scripts5.js
@@ -0,0 +1,15 @@
+function submiting() {
+         let  num1=document.forms["form2"]["first_number"].value;
+        let num2=document.forms["form2"]["second_number"].value;
+        // store input numbers
+
+
+//add two numbers
+const sum = num1 * num2;
+
+// display the sum
+
+    
+        window.alert(sum);
+    }
+    
