54a45b4729aaa539200116990ea7f3f74befefcf
souhad <souhadmoussa86@gmail.com>
2023-08-30 12:59:35 +0300

    step 4

---
+++ javascript-basics-01/scripts4.js
@@ -0,0 +1,6 @@
+function submiting() {
+    let name=document.forms["form1"]["name"].value;
+    let city=document.forms["form1"]["city"].value;
+    let surname=document.forms["form1"]["surname"].value;
+    window.alert("name"+ name + "surname/p" + surname + "city" + city );
+}
