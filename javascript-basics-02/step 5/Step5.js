20b23b6e56be32453f87a5bbe90c3e729d9c53fb
souhad <souhadmoussa86@gmail.com>
2023-08-31 13:02:02 +0300

    step 5

---
+++ javascript-basics-02/step 5/Step5.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function() {
+    let containers = document.querySelectorAll("img")
+    containers.forEach((img)=>{
+        img.addEventListener('mouseover',function(){
+img.setAttribute('src',`./images/${img.id}_2.jpg`)
+        })
+    })  
+    
+    
+})
