75cad534716dcd0e922d6df41d200bb70d66f6a0
souhad <souhadmoussa86@gmail.com>
2023-08-31 12:15:30 +0300

    step 4

--- javascript-basics-02/step 2/Step2.js
+++ javascript-basics-02/step 2/Step2.js
@@ -1,9 +1,5 @@
 
-// Target the input using querySelector
 const input = document.querySelector('#name');
-
-// Add event listener for blur event
 input.addEventListener('blur', function() {
-  // Show a dialog when the input loses focus
   alert('Thank you for participating!');
 });
--- javascript-basics-02/step 4/Step4.js
+++ javascript-basics-02/step 4/Step4.js
@@ -1,4 +1,17 @@
-function myFunction() {
-  }
+document.addEventListener("DOMContentLoaded",function(){
+  const name= document.querySelector("#name");
+  const surname= document.querySelector("#surname");
+  const city= document.querySelector("#city");
+  const reset= document.querySelector("#reset");
 
- 
+
+  reset.addEventListener('click',function(){
+    const confirmed = confirm("Are you want to reset he fileds?")
+    if(confirmed){
+      name.value='';
+      surname.value='';
+      city.value='';
+      
+    }
+  })
+})
